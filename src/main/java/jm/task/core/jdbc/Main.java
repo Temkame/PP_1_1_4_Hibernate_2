f709adc694fccdeaee2123dd1e854e99c52c1ca1
temkame <temkame@yandex.ru>
2022-03-18 17:11:39 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.awt.dnd.DragGestureEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-    }
+
+        private static final UserService userService = new UserServiceImpl();
+
+        public static void main(String[] args) {
+            userService.createUsersTable();
+            userService.saveUser("Artem", "Fokin", (byte) 20);
+            userService.saveUser("Sergey", "Lomov", (byte) 30);
+            userService.saveUser("Mihail", "Rognin", (byte) 45);
+            userService.saveUser("Evgeniy", "Artemov", (byte) 40);
+            System.out.println(userService.getAllUsers());
+//            userService.cleanUsersTable();
+//            userService.dropUsersTable();
+        }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,40 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/PP_1_1_3";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "12345";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Подключено к БД!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Не подключено к БД!");
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Подключение к БД закрыто");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Не удалось закрыть подключение");
+        }
+    }
 }
+
